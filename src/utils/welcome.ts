96ca83bd21f01ac7c49973b8257082d4f05473fb
Kamui <65237048+kamui-7@users.noreply.github.com>
2020-07-19 11:09:10 -0700

    Update welcome.ts

--- src/utils/welcome.ts
+++ src/utils/welcome.ts
@@ -4,7 +4,7 @@ const welcome = (member, channel: string, description: string): void => {
   const txtChannel: any = member.guild.channels.cache.get(channel);
   if (member.user) {
     const welcomeEmbed = new MessageEmbed()
-      .setTitle(`Welcome to The Japan Zone ${member.user.username}!`)
+      .setTitle(`Welcome to The Japan Zone, ${member.user.username}!`)
       .setDescription(description)
       .setColor('#e0b04a');
     txtChannel.send({ embed: welcomeEmbed });
